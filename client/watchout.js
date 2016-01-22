e137240877dae322adc982782e2bc7e9d1faf710
rsboggs <robertstephenboggs@gmail.com>
2016-01-22 15:29:24 -0800

    Implement array tracking bad guys

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,23 @@
 // start slingin' some d3 here.
+var createBadGuys = function() {
+  var badGuys = [];
+  for (var i = 0; i < 20; i++) {
+    var cx = Math.floor(Math.random() * 950) + 25;
+    var cy = Math.floor(Math.random() * 750) + 25;
+    badGuys.push({x: cx, y: cy});
+  }
+  return badGuys;
+};
+
+var adjustLocation = function(badGuys, callback) {
+  for (var i = 0; i < 20; i++) {
+    badGuys[i] = callback(badGuys[i]);
+  }
+};
+
+var randomLocation = function(badGuy) {
+  badGuy.x = Math.floor(Math.random() * 950) + 25;
+  badGuy.y = Math.floor(Math.random() * 750) + 25;
+};
+
+
