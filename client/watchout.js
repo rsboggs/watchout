d09a0151b9bc6c005560d6e1c5ee58ef8053a30a
rsboggs <robertstephenboggs@gmail.com>
2016-01-23 12:25:15 -0800

    feature complete

--- client/watchout.js
+++ client/watchout.js
@@ -6,19 +6,21 @@ var heroRadius = 15;
 var time = 0;
 var highScore = 0;
 var collisions = 0;
-
+var lastPosition = [];
+var collided = false;
 var createBadGuys = function() {
   var badGuys = [];
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 4; i++) {
     var cx = Math.floor(Math.random() * (width - 2 * enemyRadius)) + enemyRadius;
     var cy = Math.floor(Math.random() * (height - 2 * enemyRadius)) + enemyRadius;
-    badGuys.push({x: cx, y: cy});
+    badGuys.push({x: cx, y: cy, id: i});
   }
   return badGuys;
 };
 
 var adjustLocation = function(badGuys, callback) {
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 4; i++) {
+    lastPosition[i] = JSON.parse(JSON.stringify(badGuys[i]));
     badGuys[i] = callback(badGuys[i]);
   }
 };
@@ -29,30 +31,58 @@ var randomLocation = function(badGuy) {
   return badGuy;
 };
 
-var collisionCheck = function (badGuy) {
+var collisionCheck = function (xEnemy, yEnemy) {
       // if SQUR ROOT of: (guys[i] x - hero x)sqrd + (guys[i] y - hero y) squrd <= hero radius + enemy radius 
-      if(Math.sqrt(Math.pow((badGuy["cx"]["animVal"]["value"] - hero.attr('cx')),2) + Math.pow((badGuy["cy"]["animVal"]["value"] - hero.attr('cy')),2)) <= (heroRadius+enemyRadius)) {
+      if(Math.sqrt(Math.pow((xEnemy - hero.attr('cx')),2) + Math.pow((yEnemy - hero.attr('cy')),2)) <= (heroRadius+enemyRadius)) {
+      //  console.log(xEnemy, yEnemy, hero.attr('cx'), hero.attr('cy'));
         return true;
     }
     return false;
 };
 
+var onCollision = function() {
+  if(time>highScore) {
+     highScore = time;
+  }
+  collided = true;
+  time = 0;
+  d3.select(".highscore span").text(highScore);
+  d3.select(".current span").text(time);
+  d3.select(".collisions span").text(collisions);
+};
+
+var tracker = function() {
+  var baseX = this["cx"]["animVal"]["value"];
+  var baseY = this["cy"]["animVal"]["value"];
+
+  return function (t) {
+    var newX = this["cx"]["animVal"]["value"];
+    var newY = this["cy"]["animVal"]["value"];
+
+    var ix = d3.interpolate(baseX, newX);
+    var iy = d3.interpolate(baseY, newY);
+
+//    console.log(t);
+     if (collisionCheck(ix(t), iy(t))) {
+      onCollision();
+     }
+     var curX = this["cx"]["animVal"]["value"];
+     var curY = this["cy"]["animVal"]["value"];
+     d3.select(".x span").text(curX);
+     d3.select(".y span").text(curY);
+
+  };
+};
+
 var mapBoard = function (badGuyArray) {
+  if(collided) {
+    collisions++;
+    collided = false;
+  }
   var board = d3.select("svg").selectAll(".badGuys").data(badGuyArray);
   // UPDATE
   board.transition().duration(1000)
-    .tween('custom', function() {
-      if(collisionCheck(this)) {
-        if(time>highScore) {
-           highScore = time;
-        }
-        collisions++;
-        time = 0;
-        d3.select(".highscore span").text(highScore);
-        d3.select(".current span").text(time);
-        d3.select(".collisions span").text(collisions);
-      }
-     })
+    .tween('custom', tracker)
     .attr("cx",function (d) {return d.x;})
     .attr("cy",function (d) {return d.y;});
   // ENTER
