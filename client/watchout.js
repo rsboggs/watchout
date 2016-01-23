4dbf14a460ee62235d814e299a26987c6d1037f1
rsboggs <robertstephenboggs@gmail.com>
2016-01-22 16:07:02 -0800

    populate board with circle bad guys

--- client/watchout.js
+++ client/watchout.js
@@ -2,8 +2,8 @@
 var createBadGuys = function() {
   var badGuys = [];
   for (var i = 0; i < 20; i++) {
-    var cx = Math.floor(Math.random() * 950) + 25;
-    var cy = Math.floor(Math.random() * 750) + 25;
+    var cx = Math.floor(Math.random() * 740) + 30;
+    var cy = Math.floor(Math.random() * 540) + 30;
     badGuys.push({x: cx, y: cy});
   }
   return badGuys;
@@ -16,8 +16,26 @@ var adjustLocation = function(badGuys, callback) {
 };
 
 var randomLocation = function(badGuy) {
-  badGuy.x = Math.floor(Math.random() * 950) + 25;
-  badGuy.y = Math.floor(Math.random() * 750) + 25;
+  badGuy.x = Math.floor(Math.random() * 740) + 30;
+  badGuy.y = Math.floor(Math.random() * 540) + 30;
 };
 
+var mapBoard = function (badGuyArray) {
+  var board = d3.select("svg").selectAll("circle").data(badGuyArray);
 
+  // UPDATE
+  board.attr("cx",function (d) {return d.x;}).attr("cy",function (d) {return d.y;});
+  // ENTER
+  board.enter().append("circle")
+    .attr("cx",function (d) {return d.x;})
+    .attr("cy",function (d) {return d.y;})
+    .attr("r", "30")
+    .attr("fill","yellow")
+    .attr("stroke", "red")
+;
+  // EXIT
+  board.exit().remove();
+};
+
+var guys = createBadGuys();
+mapBoard(guys);
