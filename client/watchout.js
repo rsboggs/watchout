335b7a405cf67381f664ffbfd69a4bd0d117b1c0
rsboggs <robertstephenboggs@gmail.com>
2016-01-22 17:08:47 -0800

    draggable hero

--- client/watchout.js
+++ client/watchout.js
@@ -1,9 +1,13 @@
 // start slingin' some d3 here.
+var height = 600;
+var width = 800;
+var radius = 30;
+
 var createBadGuys = function() {
   var badGuys = [];
   for (var i = 0; i < 20; i++) {
-    var cx = Math.floor(Math.random() * 740) + 30;
-    var cy = Math.floor(Math.random() * 540) + 30;
+    var cx = Math.floor(Math.random() * (width - 2 * radius)) + radius;
+    var cy = Math.floor(Math.random() * (height - 2 * radius)) + radius;
     badGuys.push({x: cx, y: cy});
   }
   return badGuys;
@@ -16,13 +20,13 @@ var adjustLocation = function(badGuys, callback) {
 };
 
 var randomLocation = function(badGuy) {
-  badGuy.x = Math.floor(Math.random() * 740) + 30;
-  badGuy.y = Math.floor(Math.random() * 540) + 30;
+  badGuy.x = Math.floor(Math.random() * (width - 2 * radius)) + radius;
+  badGuy.y = Math.floor(Math.random() * (height - 2 * radius)) + radius;
   return badGuy;
 };
 
 var mapBoard = function (badGuyArray) {
-  var board = d3.select("svg").selectAll("circle").data(badGuyArray);
+  var board = d3.select("svg").selectAll(".badGuys").data(badGuyArray);
 
   // UPDATE
   board.transition().duration(1000).attr("cx",function (d) {return d.x;}).attr("cy",function (d) {return d.y;});
@@ -30,6 +34,7 @@ var mapBoard = function (badGuyArray) {
   board.enter().append("circle")
     .attr("cx",function (d) {return d.x;})
     .attr("cy",function (d) {return d.y;})
+    .attr('class','badGuys')
     .attr("r", "30")
     .attr("fill","yellow")
     .attr("stroke", "red")
@@ -38,9 +43,40 @@ var mapBoard = function (badGuyArray) {
   board.exit().remove();
 };
 
+
+
+var createHero = function () {
+  // var hero = data();
+  //d3.select("svg").append("cicle").attr('class','draggable');
+  // ENTER
+  var hero = d3.select("svg").selectAll(".draggable").data("hero");
+
+  var drag = d3.behavior.drag()
+  // var circle = d3.select('.draggable');
+  .on('dragstart',function () { hero.style('fill', 'white'); })
+  .on('drag', function () { hero.attr('cx',d3.event.x)
+                                  .attr('cy', d3.event.y); })
+  .on('dragend',function () { hero.style('fill', 'green'); });
+
+  hero.enter().append("circle")
+    .attr('class','draggable')
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
+    .attr('r', radius / 2)
+    .attr('fill','green')
+    .call(drag);
+};
+
 var guys = createBadGuys();
 mapBoard(guys);
+createHero();
+
+
+
+
 setInterval(function() {
   adjustLocation(guys, randomLocation);
   mapBoard(guys);
 }, 1000);
+
+// board
